569b973ec15c7eb5d6b807620099380f1afd15a7
MikeUIDev <mlindesign@gmail.com>
2022-06-09 14:23:08 -0700

    new mike lin studio development files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+
+const sass = require("sass");
+const npmSass = require("npm-sass");
+const bootstrap = require('bootstrap');
+
+const result = sass.compile("sass/_custom.scss css/main.css");
+
